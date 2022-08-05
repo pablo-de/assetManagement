f81582afed0ac2ab50317d91963097e5d058e8d9
Pablo Diaz Echeveste <diazechevestepablo@gmail.com>
2022-08-05 16:12:11 -0300

    FIX: Se arregla compose y ya conecta a la DB

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "Secret Key"
 
-MYSQL_USER = 'user1'
-MYSQL_PASS = '123456'
-MYSQL_HOST = 'localhost'
-MYSQL_DB = 'assetManagement'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/assetManagement'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{usr}:{passwd}@{host}/{db}'.format(
-    usr=MYSQL_USER, passwd=MYSQL_PASS, host=MYSQL_HOST, db=MYSQL_DB)
 app.config['SQALALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
