e1e4034a7ce9f2ad3258cff822c8e76982ff0079
Pablo <pablo@cloudshine.local>
2022-09-12 11:33:07 -0300

    FIX: reestructura

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/assetManagement'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/assetManagement'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{usr}:{passwd}@mysql:3306/{db}'.format(
+    usr=os.getenv('MYSQL_USER'), passwd=os.getenv('MYSQL_PASS'), db=os.getenv('MYSQL_DB'))
+
 
 app.config['SQALALCHEMY_TRACK_MODIFICATION'] = False
 
