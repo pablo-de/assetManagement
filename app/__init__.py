a01e60304cc1d50d3d395e8e401979920c7139de
Pablo <diazechevestepablo@gmail.com>
2022-10-19 15:22:49 -0300

    FIX: mysql connect string

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ app.secret_key = "Secret Key"
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/assetManagement'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{usr}:{passwd}@mysql:3306/{db}'.format(
-    usr=os.getenv('MYSQL_USER'), passwd=os.getenv('MYSQL_PASS'), db=os.getenv('MYSQL_DB'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{usr}:{passwd}@{mysql}:{port}/{db}'.format(
+    usr=os.getenv('MYSQL_USER'), mysql=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), passwd=os.getenv('MYSQL_PASS'), db=os.getenv('MYSQL_DB'))
 
 
 app.config['SQALALCHEMY_TRACK_MODIFICATION'] = False
